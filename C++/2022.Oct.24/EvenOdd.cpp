a1e92cf5d33704787f69f5371414878fcfb014c0
Pop-Apple <101918076+Pop-Apple@users.noreply.github.com>
2022-11-04 20:35:27 +0900

    Delete C++ directory

--- C++/2022.Oct.24/EvenOdd.cpp
+++
@@ -1,5 +0,0 @@
-if (n % 2 == 0) {
-  std::cout << "Even" << std::endl;
-} else {
-  std::cout << "Odd" << std::endl;
-}
--- C++/2022.Oct.25/Oppo.cpp
+++
@@ -1,8 +0,0 @@
-#include <iostream>
-using namespace std;
-
-int main()
-{
-    cout << "oppo" << endl;
-    return 0;
-}
