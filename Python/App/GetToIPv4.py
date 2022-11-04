1ba75f777618a89a54adb38526575c08a01f8152
Pop-Apple <101918076+Pop-Apple@users.noreply.github.com>
2022-11-04 22:56:37 +0900

    Get To IPv4

---
+++ Python/App/GetToIPv4.py
@@ -0,0 +1,17 @@
+import socket 
+ipv4 = socket.gethostbyname(socket.gethostname()) 
+#Draw IPv4
+
+#print('表示 [d]')
+
+#Menu = input('：')
+
+#if Menu == 'd':
+print(ipv4)
+
+#else:
+    #print('No :(')
+    #input()
+    #exit()
+
+input()
