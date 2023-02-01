87b62fe77517bfda0fc7ac5a32ecbd6b56bf02ea
Pop-Apple <101918076+Pop-Apple@users.noreply.github.com>
2023-02-01 19:57:21 +0900

    Create ToHex.cs

---
+++ C#/App/ToHex.cs
@@ -0,0 +1,11 @@
+public string ToHex (string inp)
+{
+  string outp = string.Empty;
+  char[] value = inp.ToCharArry();
+  foreach (char L in value)
+  {
+    int V = Convert.ToInt32(L);
+    outp+=string.Format("{0:x}",V);
+  }
+  return outp;
+}
