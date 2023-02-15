3325478a245d12c06c146fc36aebb06879407048
Pop-Apple <101918076+Pop-Apple@users.noreply.github.com>
2023-02-15 22:18:15 +0900

    XML `Entry Name` Viewer

---
+++ Python/App/XML Viewer.py
@@ -0,0 +1,23 @@
+﻿
+# XMLの中のコード名を表示
+Title = '\n' + '【 XML Entry Name Viewer 】'
+
+print(Title)
+
+def Re():
+	
+	print('\n' + '[ Enter The Directory ]' + '\n')
+
+	path=input("> ")
+
+	with open(path,encoding="utf-8") as f:
+		s=f.readlines()
+		f.close
+	for x in range(len(s)):
+		if('<entry name="' in s[x])==True:
+			Alt=s[x].replace('\n','')
+			Alt=Alt.replace('<entry name="','')
+			Alt=Alt.replace('">','')
+			print(Alt)
+	Re()
+Re()
