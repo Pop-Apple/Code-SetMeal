3f343070440afe84a6ab97bcf5d3575411191168
Pop-Apple <101918076+Pop-Apple@users.noreply.github.com>
2023-04-07 15:26:43 +0900

    Add files via upload

---
+++ C#/App/CSV-Viewer/CSV-Viewer/Main.Designer.cs
@@ -0,0 +1,127 @@
+﻿namespace CSV_Viewer
+{
+    partial class Main
+    {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージド リソースを破棄する場合は true を指定し、その他の場合は false を指定します。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.OpenButton = new System.Windows.Forms.Button();
+            this.FilePathTextBox = new System.Windows.Forms.TextBox();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.button1 = new System.Windows.Forms.Button();
+            this.progressBar = new System.Windows.Forms.ProgressBar();
+            this.CSVListBox = new System.Windows.Forms.ListBox();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // OpenButton
+            // 
+            this.OpenButton.Font = new System.Drawing.Font("MS UI Gothic", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.OpenButton.Location = new System.Drawing.Point(661, 20);
+            this.OpenButton.Name = "OpenButton";
+            this.OpenButton.Size = new System.Drawing.Size(44, 37);
+            this.OpenButton.TabIndex = 0;
+            this.OpenButton.Text = "...";
+            this.OpenButton.UseVisualStyleBackColor = true;
+            this.OpenButton.Click += new System.EventHandler(this.OpenButton_Click);
+            // 
+            // FilePathTextBox
+            // 
+            this.FilePathTextBox.Location = new System.Drawing.Point(26, 27);
+            this.FilePathTextBox.Name = "FilePathTextBox";
+            this.FilePathTextBox.ReadOnly = true;
+            this.FilePathTextBox.Size = new System.Drawing.Size(618, 25);
+            this.FilePathTextBox.TabIndex = 1;
+            this.FilePathTextBox.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // panel1
+            // 
+            this.panel1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panel1.Controls.Add(this.FilePathTextBox);
+            this.panel1.Controls.Add(this.OpenButton);
+            this.panel1.Location = new System.Drawing.Point(13, 13);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(725, 80);
+            this.panel1.TabIndex = 2;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(13, 119);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(183, 46);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "Result";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // progressBar
+            // 
+            this.progressBar.Location = new System.Drawing.Point(13, 739);
+            this.progressBar.Name = "progressBar";
+            this.progressBar.Size = new System.Drawing.Size(725, 23);
+            this.progressBar.TabIndex = 5;
+            // 
+            // CSVListBox
+            // 
+            this.CSVListBox.FormattingEnabled = true;
+            this.CSVListBox.ItemHeight = 18;
+            this.CSVListBox.Location = new System.Drawing.Point(13, 192);
+            this.CSVListBox.Name = "CSVListBox";
+            this.CSVListBox.Size = new System.Drawing.Size(725, 526);
+            this.CSVListBox.TabIndex = 6;
+            // 
+            // Main
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 18F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(752, 780);
+            this.Controls.Add(this.CSVListBox);
+            this.Controls.Add(this.progressBar);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.panel1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.Name = "Main";
+            this.ShowIcon = false;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Entry CSV   |   Sakamochanq";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button OpenButton;
+        private System.Windows.Forms.TextBox FilePathTextBox;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ProgressBar progressBar;
+        private System.Windows.Forms.ListBox CSVListBox;
+    }
+}
+
---
+++ C#/App/CSV-Viewer/CSV-Viewer/Main.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.IO;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+
+namespace CSV_Viewer
+{
+    public partial class Main : Form
+    {
+        public Main()
+        {
+            InitializeComponent();
+        }
+
+        string Path;
+
+        private void OpenButton_Click(object sender, EventArgs e)
+        {
+            using (var Dialog = new OpenFileDialog() { Filter = "CSVデータテーブル形式(*.csv) | *.csv" })
+            {
+                if (Dialog.ShowDialog() == DialogResult.OK)
+                {
+                    Path = Dialog.FileName;
+                    this.FilePathTextBox.Text = Path;
+                }
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                progressBar.Value = 0;
+                CSVListBox.Items.Clear();
+
+                string[] lines = System.IO.File.ReadAllLines(Path);
+                int rowCount = lines.Length;
+
+                progressBar.Minimum = 0;
+                progressBar.Maximum = rowCount;
+
+                foreach (string line in lines)
+                {
+                    CSVListBox.Items.Add(line);
+                    progressBar.Value++;
+                }
+
+                //MessageBox.Show("Successfully", "Entry CSV", MessageBoxButtons.OK, MessageBoxIcon.Asterisk);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+    }
+}
