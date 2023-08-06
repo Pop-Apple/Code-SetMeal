a17f1a92176ba148a8327281c57150644e89c9bc
Sakamochanq <126952815+Sakamochanq@users.noreply.github.com>
2023-08-07 00:58:02 +0900

    Add files via upload

---
+++ C#/App/Device-Module/src/Draw-CPS/Source.Designer.cs
@@ -0,0 +1,102 @@
+﻿namespace Draw_CPS
+{
+    partial class Source
+    {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージド リソースを破棄する場合は true を指定し、その他の場合は false を指定します。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.ClickButton = new System.Windows.Forms.Button();
+            this.CPSLabel = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.Timer = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // ClickButton
+            // 
+            this.ClickButton.Font = new System.Drawing.Font("UD デジタル 教科書体 NK-B", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.ClickButton.Location = new System.Drawing.Point(26, 63);
+            this.ClickButton.Name = "ClickButton";
+            this.ClickButton.Size = new System.Drawing.Size(339, 88);
+            this.ClickButton.TabIndex = 0;
+            this.ClickButton.Text = "Click";
+            this.ClickButton.UseVisualStyleBackColor = true;
+            this.ClickButton.Click += new System.EventHandler(this.ClickButton_Click);
+            // 
+            // CPSLabel
+            // 
+            this.CPSLabel.AutoSize = true;
+            this.CPSLabel.Font = new System.Drawing.Font("UD デジタル 教科書体 NK-B", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.CPSLabel.Location = new System.Drawing.Point(112, 17);
+            this.CPSLabel.Name = "CPSLabel";
+            this.CPSLabel.Size = new System.Drawing.Size(53, 31);
+            this.CPSLabel.TabIndex = 1;
+            this.CPSLabel.Text = "***";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("UD デジタル 教科書体 NK-R", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.label2.Location = new System.Drawing.Point(20, 17);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(88, 31);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "CPS：";
+            // 
+            // Timer
+            // 
+            this.Timer.Tick += new System.EventHandler(this.Timer_Tick);
+            // 
+            // Source
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ControlLight;
+            this.ClientSize = new System.Drawing.Size(390, 171);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.CPSLabel);
+            this.Controls.Add(this.ClickButton);
+            this.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.Name = "Source";
+            this.ShowIcon = false;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Draw CPS   |   Sakamochanq";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button ClickButton;
+        private System.Windows.Forms.Label CPSLabel;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Timer Timer;
+    }
+}
+
---
+++ C#/App/Device-Module/src/Draw-FPS/Source.Designer.cs
@@ -0,0 +1,83 @@
+﻿namespace Draw_FPS
+{
+    partial class Source
+    {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージド リソースを破棄する場合は true を指定し、その他の場合は false を指定します。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Source));
+            this.FPSLabel = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // FPSLabel
+            // 
+            this.FPSLabel.AutoSize = true;
+            this.FPSLabel.Font = new System.Drawing.Font("UD デジタル 教科書体 NP-B", 20F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.FPSLabel.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.FPSLabel.Location = new System.Drawing.Point(101, 88);
+            this.FPSLabel.Name = "FPSLabel";
+            this.FPSLabel.Size = new System.Drawing.Size(199, 46);
+            this.FPSLabel.TabIndex = 0;
+            this.FPSLabel.Text = "FPS: 0.0";
+            this.FPSLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(13, 13);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(0, 18);
+            this.label2.TabIndex = 1;
+            // 
+            // Source
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 18F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ScrollBar;
+            this.ClientSize = new System.Drawing.Size(582, 233);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.FPSLabel);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "Source";
+            this.ShowIcon = false;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Draw FPS   |   Sakamochanq";
+            this.Load += new System.EventHandler(this.Source_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label FPSLabel;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
